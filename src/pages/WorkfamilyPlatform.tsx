5625237cdaefd6c418f230bc58dfb89a913d9118
Tech4Humanity <62584578+TML-4PM@users.noreply.github.com>
2026-03-17 11:00:30 +1100

    feat: add WorkfamilyPlatform CC page (7 CCQs: products, grants, scorecard, idle, controller)

---
+++ src/pages/WorkfamilyPlatform.tsx
@@ -0,0 +1,4 @@
+import BridgePage from "@/components/BridgePage";
+const WorkfamilyPlatform = () => <BridgePage pageId="workfamily" title="WorkFamily Platform" icon="🤖" />;
+export default WorkfamilyPlatform;
+
